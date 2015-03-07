5422bcb92d6628407582a33261ed81438bde7733
pathrough <1598842213@qq.com>
2015-03-08 01:33:29 +0800

    add

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using BPlusTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase db = new DataBase();
+            for (; ; )
+            {
+                string input = Console.ReadLine();
+                string[] inputs = input.Split(',');
+                db.Insert(new DataItem { ID = inputs[0],Key=inputs[1],Value=inputs[2]});
+            }                
+        }
+    }
+}
