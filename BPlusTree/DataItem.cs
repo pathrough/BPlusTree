1727a072f4d80d83737873dc61a4ec97052049f0
pathrough <1598842213@qq.com>
2015-03-08 12:15:30 +0800

    add

---
+++ BPlusTree/DataItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPlusTree
+{
+    public class DataItem:IEquatable<DataItem>
+    {
+        public string ID { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public bool Equals(DataItem other)
+        {
+            return this.ID.Equals(other.ID) && this.Key.Equals(other.Key) && this.Value.Equals(other.Value);
+        }
+    }
+
+    public class DataBlock
+    {
+        private List<DataItem> _DataItemList = new List<DataItem>();
+        public List<DataItem> DataItemList
+        {
+            get { return _DataItemList; }
+            set { _DataItemList = value; }
+        }
+
+        public static int MaxItemCount
+        {
+            get
+            {
+                return 2;
+            }
+        }
+    }
+}
---
+++ BPlusTree/IndexItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPlusTree
+{
+    public class IndexItem
+    {
+        public DataBlock DataBlock { get; set; }
+        public DataItem DataItem { get; set; }//最大值
+    }
+}
