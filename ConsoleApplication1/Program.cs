d6fb681869858c6ed76f53baa4c6815d31716dd2
pathrough <1598842213@qq.com>
2015-03-08 15:30:58 +0800

    重构

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            DataBase db = new DataBase();
+            DataBase db = new OKV(2);
             for (; ; )
             {
                 string input = Console.ReadLine();
